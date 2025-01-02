e784f8f788d1539a4ff7aa4f9be5f5f041f4df11
Saumya Shah <saumyashah609@gmail.com>
2025-01-02 20:15:58 +0530

    Refactor GraphQL schema by updating type definitions and removing obsolete test file

--- src/yoga/schema.ts
+++ src/yoga/schema.ts
@@ -1,7 +1,7 @@
 import { createSchema } from "graphql-yoga";
 import { resolvers } from "./resolvers.ts";
 
-const typeDefs = await Deno.readTextFile("src/yoga/typeDef.test.gql");
+const typeDefs = await Deno.readTextFile("src/yoga/typeDef.gql");
 
 export const schema = createSchema({
     typeDefs,
